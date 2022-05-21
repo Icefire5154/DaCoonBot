fddd6f439d87219072fe70ad38fd08412a0c1552
GavinSpreck <gavin.spreck@my.uwrf.edu>
2022-05-20 20:24:38 -0500

    initial commit

---
+++ shitposts.py
@@ -0,0 +1,15 @@
+photos = [
+    "https://media.discordapp.net/attachments/974890676724564068/974890989237981244/20220121_230416.jpg",
+    "https://www.youtube.com/shorts/36x_KYU6448",
+    "https://youtu.be/AbnWq7F9o20",
+    "https://www.youtube.com/watch?v=kPhzG4UN_W0",
+    "https://www.youtube.com/watch?v=CEqNfMu02Nc",
+    "https://cdn.discordapp.com/attachments/763573359853699102/963613059295891537/ac49f04d565cda51cc8d861f65e51bd4.mp4",
+    "https://www.youtube.com/shorts/T6HDVVWan_A",
+    "https://media.discordapp.net/attachments/966539788062687252/966543207804596244/unknown.png",
+    "https://cdn.discordapp.com/attachments/763573359853699102/867232275480707082/video0-16-1-1.mov",
+    "https://youtube.com/shorts/Dmrp2spuOr0?feature=share",
+    "https://images-ext-2.discordapp.net/external/lRHqGbTNHcs-WXOlgQQnx_duGVDTG4p6nj6-HFSi3eU/https/media.discordapp.net/attachments/863185479209189404/885707315645407272/PvKOeIWq.gif",
+    "https://c.tenor.com/6GhUzdbJfmUAAAAC/nikocado-avocado-kracc-bacc.gif",
+
+]
